5599b66f7f535be957cc4bab5d79a5fa26505a5f
siemiatj <kuba@saucelabs.com>
2016-07-11 19:21:53 +0200

    - add eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
 import runSequence from 'run-sequence';
+import gulpEslint from 'gulp-eslint';
 
 var config = {
   paths: {
@@ -16,10 +17,17 @@ gulp.task('clean', () =>
   del(config.paths.js.dist)
 );
 
-gulp.task('babel-src', () =>
+gulp.task('babel-src', ['lint-src'], () =>
   gulp.src(config.paths.js.src)
     .pipe(babel())
     .pipe(gulp.dest(config.paths.js.dist))
+);
+
+gulp.task('lint-src', () =>
+  gulp.src(config.paths.js.src)
+    .pipe(gulpEslint())
+    .pipe(gulpEslint.format())
+    .pipe(gulpEslint.failAfterError())
 );
 
 // Default Task
